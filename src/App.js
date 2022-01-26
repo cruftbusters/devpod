0fcb929b1bf130a2c0c06fbff70cb66d9905ad76
tyler <tyler@cruftbusters.com>
2022-01-26 03:21:51 -0600

    title bar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 export default function App() {
-  return <>hello world</>
+  return (
+    <div style={{ backgroundColor: '#111', color: '#EEE', height: '100%' }}>
+      <div
+        style={{
+          position: 'absolute',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          fontSize: '2em',
+          margin: '0.25em',
+          color: '#FAA',
+          fontFamily: 'Titillium Web',
+        }}
+      >
+        cruftbusters llc
+      </div>
+    </div>
+  )
 }
