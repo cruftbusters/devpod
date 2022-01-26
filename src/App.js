7771db534d96676668dfec487863b7619a8489c1
tyler <tyler@cruftbusters.com>
2022-01-26 11:55:03 -0600

    email and github links

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ export default function App() {
     <div style={{ backgroundColor: '#111', color: '#EEE', height: '100%' }}>
       <div
         style={{
-          position: 'absolute',
-          left: '50%',
-          transform: 'translateX(-50%)',
           fontSize: '2em',
-          margin: '0.25em',
+          textAlign: 'center',
           color: '#FAA',
           fontFamily: 'Titillium Web',
+          padding: '0.25em',
         }}
       >
         cruftbusters llc
       </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Link href="mailto:tyler@cruftbusters.com">email us</Link>
+        <Link href="https://github.com/cruftbusters">github</Link>
+      </div>
     </div>
+  )
+}
+
+function Link({ href, children }) {
+  return (
+    <a
+      href={href}
+      style={{
+        display: 'block',
+        fontSize: '1.75em',
+        color: '#AFF',
+        backgroundColor: '#333',
+        padding: '0.125em 0.75em',
+        borderRadius: '0.125em',
+        textDecoration: 'none',
+        margin: '0 0.125em',
+      }}
+    >
+      {children}
+    </a>
   )
 }
