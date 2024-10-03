2eb70e776e32a409acabb1ec27b1a6850dee61c6
tyler <tyler@cruftbusters.com>
2024-10-02 20:38:24 -0700

    update home page descriptions of what the biz is

--- apps/home/src/App.tsx
+++ apps/home/src/App.tsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ margin: '1em' }}>
+        <h1>Cruftbusters LLC</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div style={{ margin: '0.5em 1em' }}>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          We are a software consultancy based in Bothell, WA, USA with focus on
+          building relationships, taking many more much smaller steps, and
+          optimizing for delivery.
+        </p>
+        <p>
+          This team has helped build products and organizations of all sizes
+          across the Midwest. These companies include leaders in financial
+          services, IoT, retail, and pharmaceutical benefits. We advise
+          executives towards strengthing relationships with customers while
+          refining and implementing their strategic vision.
+        </p>
+        <p>
+          At Cruftbusters people not a means but an end in themselves. We
+          practice this philosphy every day through rapid prototyping, pair
+          programming, and active community building. We are "full-stack" in
+          that we are engineers, builders, and business operators. Through our
+          rich experience we help make the connections your business needs to
+          efficiently and sustainably reach the market.
+        </p>
+        <p>
+          Does our approach speak to you? Please reach out via our contact info
+          below for an introduction. We are happy to advise about new
+          opportunities to grow your business!
+        </p>
+        <p>
+          Are you interested in joining a team that builds things together?
+          Please reach out to learn about highly-collaborative opportunities to
+          work with us!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{ margin: '0.5em 1em' }}>
+        <h2>Useful Links</h2>
+        <div>
+          <a href="https://github.com/cruftbusters">our github organization</a>
+        </div>
+        <a
+          hidden={true}
+          href="https://ccfs.sos.wa.gov/#/expressAnnualReport/1593817"
+        >
+          WA UBI 604 960 276
+        </a>
+      </div>
+      <div style={{ margin: '0.5em 1em' }}>
+        <h2>Meet the Team</h2>
+        Tyler Johnson, Principal
+        <br />
+        <div>
+          <a href="mailto:tyler@cruftbusters.com">tyler@cruftbusters.com</a>
+        </div>
+        <div>
+          <a href="tel:+16123562513">(612) 356-2513</a>
+        </div>
+      </div>
     </>
   )
 }
