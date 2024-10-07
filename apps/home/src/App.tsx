a96fd2919f271140d45a265638d22a5e3d6f9776
tyler <tyler@cruftbusters.com>
2024-10-07 09:28:30 -0700

    shorter and sweeter

--- apps/home/src/App.tsx
+++ apps/home/src/App.tsx
@@ -6,35 +6,12 @@ function App() {
       </div>
       <div style={{ margin: '0.5em 1em' }}>
         <p>
-          We are a software consultancy based in Bothell, WA, USA with focus on
-          building relationships, taking many more much smaller steps, and
-          optimizing for delivery.
+          Building a product is a lot like playing in a jazz band. First we
+          listen to what is going on around us. Then we perform what comes to
+          us. After a few small iterations of this hopefully the whole room is
+          grooving otherwise we try something new.
         </p>
-        <p>
-          This team has helped build products and organizations of all sizes
-          across the Midwest. These companies include leaders in financial
-          services, IoT, retail, and pharmaceutical benefits. We advise
-          executives towards strengthing relationships with customers while
-          refining and implementing their strategic vision.
-        </p>
-        <p>
-          At Cruftbusters people not a means but an end in themselves. We
-          practice this philosphy every day through rapid prototyping, pair
-          programming, and active community building. We are "full-stack" in
-          that we are engineers, builders, and business operators. Through our
-          rich experience we help make the connections your business needs to
-          efficiently and sustainably reach the market.
-        </p>
-        <p>
-          Does our approach speak to you? Please reach out via our contact info
-          below for an introduction. We are happy to advise about new
-          opportunities to grow your business!
-        </p>
-        <p>
-          Are you interested in joining a team that builds things together?
-          Please reach out to learn about highly-collaborative opportunities to
-          work with us!
-        </p>
+        <p>Interested in jamming together? Say hi via my contacts below</p>
       </div>
       <div style={{ margin: '0.5em 1em' }}>
         <h2>Useful Links</h2>
