07430c326b3b2fc4c36bc874b96f0cda3f8b751a
tyler <tyler@cruftbusters.com>
2024-11-17 00:49:43 +0000

    refactor

--- apps/home/src/TransferEditor.tsx
+++ apps/home/src/TransferEditor.tsx
@@ -1,6 +1,7 @@
 import { database, Ledger, Transfer } from './database'
 import { v4 as uuidv4 } from 'uuid'
 import { Grid, GridRow } from './Grid'
+import { MarginAboveBelow } from './MarginAround'
 
 export function TransferEditor({
   ledger,
@@ -11,12 +12,10 @@ export function TransferEditor({
     ledger === undefined || (
       <>
         <h3>Transfer Editor</h3>
-        {ledger?.transfers === undefined || ledger?.transfers?.length < 1 || (
-          <div>
-            <TransferListEditorGrid ledger={ledger} />
-          </div>
-        )}
-        <div style={{ margin: '1em 0' }}>
+        <MarginAboveBelow>
+          <TransferListEditorGrid ledger={ledger} />
+        </MarginAboveBelow>
+        <MarginAboveBelow>
           <button
             onClick={() => {
               database.transfers.add({
@@ -30,9 +29,9 @@ export function TransferEditor({
             }}
             style={{ padding: '0.25em 0.5em' }}
           >
-            Add transfer
+            Create new transfer
           </button>
-        </div>
+        </MarginAboveBelow>
       </>
     )
   )
@@ -44,64 +43,67 @@ function TransferListEditorGrid({
   ledger: Ledger & { transfers: Transfer[] }
 }) {
   return (
-    <Grid
-      style={{
-        gridTemplateColumns: 'repeat(5, auto)',
-      }}
-    >
-      <GridRow>
-        <div>Date</div>
-        <div>Debit Account</div>
-        <div>Credit Account</div>
-        <div>Amount</div>
-      </GridRow>
-      {ledger.transfers.map((transfer) => (
-        <GridRow key={transfer.key}>
-          <input
-            onChange={(e) =>
-              database.transfers.put({
-                ...transfer,
-                date: e.target.value,
-              })
-            }
-            placeholder="date"
-            value={transfer.date}
-          />
-          <input
-            onChange={(e) =>
-              database.transfers.put({
-                ...transfer,
-                debitAccount: e.target.value,
-              })
-            }
-            placeholder="debit account"
-            value={transfer.debitAccount}
-          />
-          <input
-            onChange={(e) =>
-              database.transfers.put({
-                ...transfer,
-                creditAccount: e.target.value,
-              })
-            }
-            placeholder="credit account"
-            value={transfer.creditAccount}
-          />
-          <input
-            onChange={(e) =>
-              database.transfers.put({
-                ...transfer,
-                amount: parseInt(e.target.value),
-              })
-            }
-            placeholder="amount"
-            value={transfer.amount === 0 ? '' : transfer.amount.toString()}
-          />
-          <button onClick={() => database.transfers.delete(transfer.key)}>
-            &times;
-          </button>
+    ledger.transfers === undefined ||
+    ledger.transfers?.length < 1 || (
+      <Grid
+        style={{
+          gridTemplateColumns: 'repeat(5, auto)',
+        }}
+      >
+        <GridRow>
+          <div>Date</div>
+          <div>Debit Account</div>
+          <div>Credit Account</div>
+          <div>Amount</div>
         </GridRow>
-      ))}
-    </Grid>
+        {ledger.transfers.map((transfer) => (
+          <GridRow key={transfer.key}>
+            <input
+              onChange={(e) =>
+                database.transfers.put({
+                  ...transfer,
+                  date: e.target.value,
+                })
+              }
+              placeholder="date"
+              value={transfer.date}
+            />
+            <input
+              onChange={(e) =>
+                database.transfers.put({
+                  ...transfer,
+                  debitAccount: e.target.value,
+                })
+              }
+              placeholder="debit account"
+              value={transfer.debitAccount}
+            />
+            <input
+              onChange={(e) =>
+                database.transfers.put({
+                  ...transfer,
+                  creditAccount: e.target.value,
+                })
+              }
+              placeholder="credit account"
+              value={transfer.creditAccount}
+            />
+            <input
+              onChange={(e) =>
+                database.transfers.put({
+                  ...transfer,
+                  amount: parseInt(e.target.value),
+                })
+              }
+              placeholder="amount"
+              value={transfer.amount === 0 ? '' : transfer.amount.toString()}
+            />
+            <button onClick={() => database.transfers.delete(transfer.key)}>
+              &times;
+            </button>
+          </GridRow>
+        ))}
+      </Grid>
+    )
   )
 }
