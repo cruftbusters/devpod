bea3a648cb400573df2240e7c0a197942d5f0ea8
tyler <tyler@cruftbusters.com>
2024-11-23 08:04:13 +0000

    add nginx to devcontainer

--- .devcontainer/entrypoint.zsh
+++ .devcontainer/entrypoint.zsh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
+
 workspace=/root/cruftbusters.com
 ln -sf $workspace/.config /root/.config
+ln -sf $workspace/.devcontainer/nginx.conf /etc/nginx/nginx.conf
+
+nginx
 
 git config --global core.editor nvim
 git config --global init.defaultBranch main
@@ -12,8 +16,8 @@ git clone git@github.com:cruftbusters/cruftbusters.com.git $workspace
 cd $workspace
 pnpm -r install
 
-screen -d -m -S home-dev pnpm -F home dev --host
+screen -d -m -S home-dev pnpm -F home dev
 screen -d -m -S home-test pnpm -F home exec vitest --ui --api 5174
-screen -d -m -S home-e2e pnpm -F home exec playwright test --ui --ui-host 0.0.0.0 --ui-port 5175
+screen -d -m -S home-e2e pnpm -F home exec playwright test --ui --ui-port 5175
 
 /bin/zsh
--- .devcontainer/run.zsh
+++ .devcontainer/run.zsh
@@ -1,7 +1,5 @@
 docker run -it --rm \
-  -p 5173:5173 \
-  -p 5174:5174 \
-  -p 5175:5175 \
+  -p 8080:8080 \
   -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
   -v $HOME/.gitconfig:/root/.gitconfig:ro \
   -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
