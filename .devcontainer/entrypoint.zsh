0125cbef8d9697c35cd7413b0d7a095d68018de5
tyler <tyler@cruftbusters.com>
2024-11-28 21:46:48 +0000

    set git config local to devcontainer

--- .devcontainer/entrypoint.zsh
+++ .devcontainer/entrypoint.zsh
@@ -5,15 +5,14 @@ workspace=/root/cruftbusters.com
 ln -sf $workspace/.config /root/.config
 ln -sf $workspace/.devcontainer/nginx.conf /etc/nginx/nginx.conf
 
-nginx
-
-git config --global core.editor nvim
-git config --global init.defaultBranch main
-git config --global pull.rebase true
-git config --global push.autoSetupRemote true
-
 git clone git@github.com:cruftbusters/cruftbusters.com.git $workspace
 cd $workspace
+
+git config core.editor nvim
+git config init.defaultBranch main
+git config pull.rebase true
+git config push.autoSetupRemote true
+
 pnpm -r install
 
 screen -d -m -S home-dev pnpm -F home dev
