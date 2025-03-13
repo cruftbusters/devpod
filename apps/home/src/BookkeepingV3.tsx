d4c9c0fa47fbfb07f5ef968a386eb964a23f79a4
tyler <tyler@cruftbusters.com>
2025-03-13 00:41:09 +0000

    refactor to extract JournalSummary

--- apps/home/src/BookkeepingV3.tsx
+++ apps/home/src/BookkeepingV3.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useMemo, useState } from 'react'
+import { useState } from 'react'
 import { useLiveQuery } from 'dexie-react-hooks'
 
 import { MarginAround } from './MarginAround'
@@ -8,6 +8,7 @@ import { database, LiveJournal } from './LiveJournal'
 import { Journal } from './Journal'
 import { TransferArraySheet } from './Transfer'
 import { SheetSerde } from './SheetSerde'
+import { JournalSummary } from './JournalSummary'
 
 export function BookkeepingV3() {
   const [key, setKey] = useState('')
@@ -259,85 +260,6 @@ function JournalImportExport({ liveJournal }: { liveJournal: LiveJournal }) {
         </Listicle>
       </p>
       <p>{status.message}</p>
-    </>
-  )
-}
-
-function JournalSummary({ journal }: { journal: Journal }) {
-  const status = useStatus()
-  const summary = useMemo(() => {
-    try {
-      const summary = journal.summary((date) => date?.substring(0, 4))
-      status.info('successfully summarized journal')
-      return summary
-    } catch (cause) {
-      status.error('failed to summarize journal', cause)
-      return { accounts: [], periods: [] }
-    }
-  }, [status, journal])
-
-  return (
-    <>
-      <h3>summary</h3>
-      <p>{status.message}</p>
-      <div
-        aria-label="summary"
-        className="grid alternating"
-        style={{
-          display: 'grid',
-          gridTemplateColumns: `repeat(${1 + 2 * summary.periods.length}, auto)`,
-          gridGap: '0 1em',
-          textWrap: 'nowrap',
-          overflowX: 'auto',
-        }}
-      >
-        <div
-          className="grid-row"
-          style={{
-            display: 'grid',
-            gridColumn: '1/-1',
-            gridTemplateColumns: 'subgrid',
-          }}
-        >
-          <div>account</div>
-          {summary.periods.map((period) => (
-            <Fragment key={period}>
-              <div>{period}</div>
-              <div />
-            </Fragment>
-          ))}
-        </div>
-        {summary.accounts.map(({ path, snapshots }) => (
-          <div
-            aria-label={path.join(':')}
-            className="grid-row"
-            key={path.join(':')}
-            style={{
-              display: 'grid',
-              gridColumn: '1/-1',
-              gridTemplateColumns: 'subgrid',
-            }}
-          >
-            <div style={{ textIndent: `calc(${path.length - 1} * 1em)` }}>
-              {path[path.length - 1]}
-            </div>
-            {summary.periods.map((period, index) => {
-              const amount = snapshots[index]
-              const nextAmount = snapshots[index + 1]
-              return (
-                <Fragment key={period}>
-                  <div>{amount?.format()}</div>
-                  <div>
-                    {amount &&
-                      nextAmount &&
-                      nextAmount.plus(amount.negate()).format()}
-                  </div>
-                </Fragment>
-              )
-            })}
-          </div>
-        ))}
-      </div>
     </>
   )
 }
---
+++ apps/home/src/JournalSummary.tsx
@@ -0,0 +1,82 @@
+import { useMemo, Fragment } from 'react'
+import { useStatus } from './bookkeeping_v2/useStatus'
+import { Journal } from './Journal'
+
+export function JournalSummary({ journal }: { journal: Journal }) {
+  const status = useStatus()
+  const summary = useMemo(() => {
+    try {
+      const summary = journal.summary((date) => date?.substring(0, 4))
+      status.info('successfully summarized journal')
+      return summary
+    } catch (cause) {
+      status.error('failed to summarize journal', cause)
+      return { accounts: [], periods: [] }
+    }
+  }, [status, journal])
+
+  return (
+    <>
+      <h3>summary</h3>
+      <p>{status.message}</p>
+      <div
+        aria-label="summary"
+        className="grid alternating"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${1 + 2 * summary.periods.length}, auto)`,
+          gridGap: '0 1em',
+          textWrap: 'nowrap',
+          overflowX: 'auto',
+        }}
+      >
+        <div
+          className="grid-row"
+          style={{
+            display: 'grid',
+            gridColumn: '1/-1',
+            gridTemplateColumns: 'subgrid',
+          }}
+        >
+          <div>account</div>
+          {summary.periods.map((period) => (
+            <Fragment key={period}>
+              <div>{period}</div>
+              <div />
+            </Fragment>
+          ))}
+        </div>
+        {summary.accounts.map(({ path, snapshots }) => (
+          <div
+            aria-label={path.join(':')}
+            className="grid-row"
+            key={path.join(':')}
+            style={{
+              display: 'grid',
+              gridColumn: '1/-1',
+              gridTemplateColumns: 'subgrid',
+            }}
+          >
+            <div style={{ textIndent: `calc(${path.length - 1} * 1em)` }}>
+              {path[path.length - 1]}
+            </div>
+            {summary.periods.map((period, index) => {
+              const amount = snapshots[index]
+              const nextAmount = snapshots[index + 1]
+              return (
+                <Fragment key={period}>
+                  <div>{amount?.format()}</div>
+                  <div>
+                    {amount &&
+                      nextAmount &&
+                      nextAmount.plus(amount.negate()).format()}
+                  </div>
+                </Fragment>
+              )
+            })}
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
