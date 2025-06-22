68f582dbdd251359cde84314bf14dcb87d181275
arctair <jtyler_@outlook.com>
2025-06-22 05:26:19 +0000

    drop web

--- apps/home/src/MarginAround.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export function MarginAround({ children }: PropsWithChildren) {
-  return <div style={{ margin: '1em' }}>{children}</div>
-}
-
-export function MarginAboveBelow({ children }: PropsWithChildren) {
-  return <div style={{ margin: '1em 0' }}>{children}</div>
-}
